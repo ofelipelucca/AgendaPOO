46f3235ff1dd1a1505fb57205a49d5b28e0f2062
Samuel Horta <samuel.horta18@gmail.com>
2024-05-28 20:44:43 -0300

    corrigindo erros

--- Implementações/Tarefa/Tarefa.py
+++ Implementações/Tarefa/Tarefa.py
@@ -120,6 +120,95 @@ class Compromisso(Inter_Compromisso):
         self._local = local
         self._horario = horario
     
+    def getTitulo(self) -> str:
+        return self._titulo
+
+    # @brief Busca pela descricao da tarefa
+    #
+    # @return String da destricao
+    def getDescricao(self) -> str:
+        return self._descricao
+
+    # @brief Busca pela data da tarefa
+    #
+    # @return String da data
+    def getData(self) -> str:
+        return self._data
+
+    # @brief Busca pela prioridade da tarefa
+    #
+    # @return Unsigned (1, 2 ou 3)
+    def getPrioridade(self) -> str:
+        return self._prioridade
+
+    # @brief Busca pelo estado da tarefa
+    #
+    # @return String do estado
+    def getEstado(self) -> str:
+        return self._estado
+
+    # @brief Muda o titulo de uma tarefa
+    #
+    # @param novo_Titulo O novo titulo 
+    def setTitulo(self, novo_Titulo: str) -> None:
+        MAX_CARACTERES = 50
+
+        if novo_Titulo <= MAX_CARACTERES or not novo_Titulo:
+            self._titulo = novo_Titulo
+        else:
+            print("Titulo invalido!")
+            print("O titulo deve possuir entre 1 e 50 caracteres")
+
+    # @brief Muda a descricao de uma tarefa
+    #
+    # @param nova_Descricao A nova descricao
+    def setDescricao(self, nova_Descricao: str) -> None:
+        MAX_CARACTERES = 120
+
+        if nova_Descricao <= MAX_CARACTERES or not nova_Descricao:
+            self._descricao = nova_Descricao
+        else:
+            print("Descricao invalida!")
+            print("A descricao deve possuir entre 1 e 120 caracteres")
+
+    # @brief Muda a data de uma tarefa
+    #
+    # @param nova_Data a nova data 
+    #
+    # @attention A data informada deve estar no formato: DD/MM/AAAA
+    def setData(self, nova_Data: str) -> None:
+        pass
+
+    # @brief Muda a prioridade de uma tarefa
+    #
+    # @param nova_Prioridade A nova prioridade 
+    def setPrioridade(self, nova_Prioridade: int) -> None:
+        if nova_Prioridade == 1 or nova_Prioridade == 2 or nova_Prioridade == 3:
+            self._prioridade = nova_Prioridade
+        else:
+            print("Prioridade invalida!")
+            print("A prioridade deve ser:")
+            print(" - '1' para mais importante")
+            print(" - '2' para importante")
+            print(" - '2' para menos importante")
+
+    # @brief Muda o estado de uma tarefa
+    #
+    # @param novo_Estado O novo estado
+    def setEstado(self, novo_Estado) -> None:
+        estados = {
+            "feito",
+            "em progresso",
+            "não feito"
+        }
+        if novo_Estado in estados:
+            self._estado = novo_Estado
+        else:
+            print("Estado invalido!")
+            print("O estado deve ser:")
+            for estado in estados:
+                print(f"    - '{estado}'")
+
     # @brief Busca pela cor do compromisso
     #
     # @return String com o codigo ANSI da cor
--- Teste/Testes_Notificação.py
+++ Teste/Testes_Notificação.py
@@ -3,11 +3,11 @@ from Implementações.Tarefa.Tarefa import Compromisso
 from Implementações.Lembrete import Lembrete
 def main():
     # Criar instâncias de Compromisso e Lembrete
-    compromisso = Compromisso("Reunião de projeto", "14:30")
-    lembrete = Lembrete("Comprar presente", "16:00")
+    compromisso = Compromisso("Reunião de projeto", "Discussão sobre o projeto", "25/12/2024", 1, "em progresso", "azul", "Sala de Reunião", "14:30:00")
+    lembrete = Lembrete("25/12/2024", "Comprar presente", "16:00:00")
 
     # Criar instância de Notificacao
-    notificacao = Notificação(hora_antes=1, min_antes=0)
+    notificacao = Notificação(minutos=30, horas=1)
 
     # Ativar notificação
     notificacao.ativarNotificacao()
