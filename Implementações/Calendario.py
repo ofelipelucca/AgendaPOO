a4f2327b520562130cb3d386295c3cfc67d22971
rob1913r <robertojuniorme@gmail.com>
2024-05-28 14:25:29 -0300

    Update Calendario.py

--- Implementações/Calendario.py
+++ Implementações/Calendario.py
@@ -8,12 +8,6 @@ class Calendario(Inter_Calendario):
         self.__mapa_Horario: Dict[str, List[str]] = {}
         self.__agenda: Dict[str, Dict[str, List[str]]] = {}
 
-
-    # @brief Insere um grupo de atividades em um determinado horário
-    #
-    # @param key O horario das atividades ou o dia se for chamada com um parâmetro só
-    #
-    # @param atividades As atividades que serão inseridas
     def inserir(self, key: str, atividades: List[str] = []) -> None:
         if len(atividades) == 0:
             #Implementa o inserir só com um parâmetro
@@ -34,31 +28,17 @@ class Calendario(Inter_Calendario):
                 
             self.__mapa_Horario[key].extend(atividades)
 
-
-    # @brief Calcula o tamanho do mapa de horários
-    #
-    # @return Um int com o tamanho
     def sizeMapaHorario(self) -> int:
         return len(self.__mapa_Horario)
 
-
-    # @brief Calcula o tamanho da agenda
-    #
-    # @return Um int com o tamanho
     def sizeAgenda(self) -> int:
         return len(self.__agenda)
     
-    
-    # @brief Ordena a agenda de forma crescente de horários
     def ordenarAgenda(self) -> None:
         for dia in self.__agenda:
             self.__agenda[dia] = sorted(self.__agenda[dia].items(), key=lambda x: datetime.strptime(x[0], "%H:%M"))
             self.__agenda[dia] = {key: valor for key, valor in self.__agenda[dia]}
     
-
-    # @brief Imprime o calendario de 7 dias a partir do dia escolhido (Contando com ele)
-    #
-    # @param dia_inicial O primero dia da sequência a ser mostrada
     def imprimirCalendario(self, dia_inicial: str) -> None:
         data_inicial = datetime.strptime(dia_inicial, "%d/%m/%Y")
 
