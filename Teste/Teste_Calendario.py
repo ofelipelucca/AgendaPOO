9a95f0e91367f4845877aeabb498d65edb44c5fe
rob1913r <robertojuniorme@gmail.com>
2024-06-05 15:42:13 -0300

    Update Teste_Calendario.py

--- Teste/Teste_Calendario.py
+++ Teste/Teste_Calendario.py
@@ -6,11 +6,22 @@ def main():
     meu_calendario = Calendario()
 
     # Inserindo algumas atividades
-    meu_calendario.inserir("01/06/2024", ["Estudar Python", "Fazer exercícios"])
-    meu_calendario.inserir("02/06/2024", ["Reunião", "Almoçar com amigos"])
-    meu_calendario.inserir("03/06/2024", ["Ir ao supermercado", "Ler um livro"])
-    meu_calendario.inserir("04/06/2024", ["Assistir filme", "Jogar videogame"])
-    meu_calendario.inserir("05/06/2024", ["Limpar a casa", "Fazer compras"])
+    meu_calendario.inserir("10:00", ["Estudar Python", "Fazer exercícios"])
+    meu_calendario.inserir("20:00", "Fazer trabalho de POO")
+    meu_calendario.inserir("01/06/2024")
+    
+    meu_calendario.inserir("15:30", ["Reunião", "Almoçar com amigos"])
+    meu_calendario.inserir("02/06/2024")
+    
+    meu_calendario.inserir("16:00", "Ir ao supermercado")
+    meu_calendario.inserir("16:00", "Ler um livro")
+    meu_calendario.inserir("03/06/2024")
+    
+    meu_calendario.inserir("18:00", ["Assistir filme", "Jogar videogame"])
+    meu_calendario.inserir("04/06/2024")
+    
+    meu_calendario.inserir("14:00", ["Limpar a casa", "Fazer compras"])
+    meu_calendario.inserir("05/06/2024")
 
     # Ordenando a agenda
     meu_calendario.ordenarAgenda()
