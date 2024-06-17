04b18a10579d1f06fec700633a26b1027df33ade
ofelipelucca <felipe.lucca.t.oliveira@gmail.com>
2024-06-17 03:01:37 -0300

    Alterando a janela incial

--- src/UI/UI_Principal.py
+++ src/UI/UI_Principal.py
@@ -45,7 +45,7 @@ class Principal(tk.Tk):
 
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.mostrar_tela("Login")
+        self.mostrar_tela("Register")
 
     def mostrar_tela(self, pagina_nome):
         frame = self.frames[pagina_nome]
