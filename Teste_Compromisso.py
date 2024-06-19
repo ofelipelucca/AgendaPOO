a9c09144a2a1d85f1294607466b3775d9d96261b
ofelipelucca <felipe.lucca.t.oliveira@gmail.com>
2024-06-19 14:16:09 -0300

    Atualizando testes

---
+++ Teste_Compromisso.py
@@ -0,0 +1,42 @@
+from src.Implementações.Compromisso.Compromisso import Compromisso
+from src.Implementações.Compromisso.ListaCompromisso import ListaCompromisso
+
+def main():
+    # Criando uma lista de compromissos
+    lista_compromissos = ListaCompromisso()
+
+    # Criando um compromisso
+    compromisso1 = Compromisso("Reunião", "Reunião sobre o projeto X", "15/06/2024", 2, "não feito", "laranja", "Escritório", "09:00:00")
+
+    # Adicionando o compromisso à lista
+    lista_compromissos.adicionarCompromisso(compromisso1, "user1@example.com")
+
+    # Buscando o compromisso pelo título
+    compromisso_encontrado = lista_compromissos.buscarCompromisso("user1@example.com", "Reunião")
+    if compromisso_encontrado:
+        print("Compromisso encontrado:")
+        print("Título:", compromisso_encontrado['Título'])
+        print("Descrição:", compromisso_encontrado['Descrição'])
+        print("Data:", compromisso_encontrado['Data'])
+        print("Prioridade:", compromisso_encontrado['Prioridade'])
+        print("Estado:", compromisso_encontrado['Estado'])
+        print("Cor:", compromisso_encontrado['Cor'])
+        print("Local:", compromisso_encontrado['Local'])
+        print("Horário:", compromisso_encontrado['Horário'])
+    else:
+        print("Compromisso não encontrado.")
+
+    # Removendo o compromisso
+    print("Removendo o compromisso...")
+    lista_compromissos.removerCompromisso(compromisso1, "user1@example.com")
+
+    # Buscando o compromisso removido
+    compromisso_removido = lista_compromissos.buscarCompromisso("user1@example.com", "Reunião")
+
+    if compromisso_removido:
+        print("Compromisso encontrado após remoção:", compromisso_removido.getTitulo())
+    else:
+        print("Compromisso não encontrado após remoção.")
+
+if __name__ == "__main__":
+    main()
--- Teste_Tarefa.py
+++ Teste_Tarefa.py
@@ -1,4 +1,4 @@
-from src.Implementações.Tarefa.Tarefa import Tarefa, Compromisso
+from src.Implementações.Tarefa.Tarefa import Tarefa
 from src.Implementações.Tarefa.ListaTarefa import ListaTarefa
 
 def main():
@@ -6,19 +6,19 @@ def main():
     lista_tarefas = ListaTarefa()
 
     # Criando uma tarefa
-    tarefa1 = Tarefa("Fazer compras", "Ir ao supermercado comprar alimentos", "10/06/2024", 2, "não feito")
+    tarefa1 = Tarefa("Fazer compras", "Comprar alimentos", "10/06/2024", 2, "não feito")
 
     # Adicionando a tarefa à lista
     lista_tarefas.adicionarTarefa(tarefa1, "user1@example.com")
 
     # Criando outra tarefa
-    tarefa2 = Tarefa("Estudar Python", "Estudar programação em Python", "15/06/2024", 1, "em progresso")
+    tarefa2 = Tarefa("Estudar Python", "Estudar programação", "15/06/2024", 1, "em progresso")
 
     # Adicionando a segunda tarefa à lista
     lista_tarefas.adicionarTarefa(tarefa2, "user1@example.com")
 
     # Buscando a tarefa pelo título
-    tarefa_encontrada = lista_tarefas.buscarTarefa("Fazer compras")
+    tarefa_encontrada = lista_tarefas.buscarTarefa("user1@example.com", "Fazer compras")
     if tarefa_encontrada:
         print("Tarefa encontrada:")
         print("Título:", tarefa_encontrada['Título'])
@@ -30,41 +30,15 @@ def main():
         print("Tarefa não encontrada.")
 
     # Removendo a primeira tarefa
+    print("Removendo a terafa...")
     lista_tarefas.removerTarefa(tarefa1, "user1@example.com")
 
     # Buscando a tarefa removida
-    tarefa_removida = lista_tarefas.buscarTarefa("Fazer compras")
+    tarefa_removida = lista_tarefas.buscarTarefa("user1@example.com", "Fazer compras")
     if tarefa_removida:
         print("Tarefa encontrada após remoção:", tarefa_removida.getTitulo())
     else:
         print("Tarefa não encontrada após remoção.")
-
-    # Criando uma lista de compromissos
-    lista_compromissos = ListaTarefa()
-
-    # Criando um compromisso
-    compromisso1 = Compromisso("Reunião de trabalho", "Reunião sobre o projeto X", "15/06/2024", 2, "não feito", "laranja", "Escritório", "09:00:00")
-
-    # Adicionando o compromisso à lista
-    lista_compromissos.adicionarTarefa(compromisso1, "user1@example.com")
-
-    # Buscando o compromisso pelo título
-    compromisso_encontrado = lista_compromissos.buscarTarefa("Reunião de trabalho")
-    if compromisso_encontrado:
-        print("Compromisso encontrado:")
-        print("Título:", compromisso_encontrado['Título'])
-        print("Descrição:", compromisso_encontrado['Descrição'])
-        print("Data:", compromisso_encontrado['Data'])
-        print("Prioridade:", compromisso_encontrado['Prioridade'])
-        print("Estado:", compromisso_encontrado['Estado'])
-        print("Cor:", compromisso_encontrado['Cor'])
-        print("Local:", compromisso_encontrado['Local'])
-        print("Horário:", compromisso_encontrado['Horário'])
-    else:
-        print("Compromisso não encontrado.")
-
-    # Removendo o compromisso
-    lista_compromissos.removerTarefa(compromisso1, "user1@example.com")
 
 if __name__ == "__main__":
     main()
